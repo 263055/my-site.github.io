5dbaae1ecb1d4288f942c8f1f7b66c092a1c2c85
wzy <2630559606@qq.com>
2023-01-18 13:11:00 +0800

    修改部分bug，添加注释

--- src/main/java/cn/luischen/api/QiniuCloudService.java
+++ src/main/java/cn/luischen/api/QiniuCloudService.java
@@ -8,6 +8,7 @@ import com.qiniu.common.QiniuException;
 import com.qiniu.common.Zone;
 import com.qiniu.http.Response;
 import com.qiniu.storage.Configuration;
+import com.qiniu.storage.Region;
 import com.qiniu.storage.UploadManager;
 import com.qiniu.storage.model.DefaultPutRet;
 import com.qiniu.util.Auth;
@@ -47,8 +48,10 @@ public class QiniuCloudService {
 
     public String upload(MultipartFile file, String fileName) {
 
-        //构造一个带指定Zone对象的配置类
-        Configuration cfg = new Configuration(Zone.zone0());
+        //构造一个带指定 Region 对象的配置类
+        Configuration cfg = new Configuration(Region.huanan());
+//        //构造一个带指定Zone对象的配置类
+//        Configuration cfg = new Configuration(Zone.zone0());
         //...其他参数参考类注释
         UploadManager uploadManager = new UploadManager(cfg);
         //默认不指定key的情况下，以文件内容的hash值作为文件名
